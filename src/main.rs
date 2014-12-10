1bc1b6a4c67dd08d44d1ba6fca6610581acaaad3
logaritmisk <anders.e.olsson@gmail.com>
2014-12-10 20:46:52 +0100

    kill all whitespaces

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ const MS_PER_UPDATE : uint = 10;
 
 fn main() {
     sdl2::init(sdl2::INIT_EVERYTHING);
-    
+
     let window = match Window::new("SMB", WindowPos::PosCentered, WindowPos::PosCentered, SCREEN_WIDTH, SCREEN_HEIGHT, OPENGL) {
         Ok(window) => window,
         Err(err)   => panic!("failed to create window: {}", err)
@@ -25,9 +25,9 @@ fn main() {
         Ok(renderer) => renderer,
         Err(err)     => panic!("failed to create renderer: {}", err)
     };
-        
+
     let mut player = Rect::new(0, 290, 10, 10);
-    
+
     let mut x : f32 = 390.0;
     let mut y : f32 = 290.0;
 
@@ -36,11 +36,11 @@ fn main() {
 
     let mut on_ground = true;
     let mut gravity : f32 = 0.3;
-    
+
     let mut current : uint = 0;
     let mut elapsed : uint = 0;
     let mut lag : uint = 0;
-    
+
     let mut previous : uint = get_ticks();
 
     'event : loop {
@@ -61,7 +61,7 @@ fn main() {
                 } else if key == sdl2::keycode::KeyCode::Up {
                     if on_ground {
                         v_y = -8.0;
-                        
+
                         on_ground = false;
                     }
                 }
@@ -79,17 +79,17 @@ fn main() {
             },
             _ => (),
         }
-        
+
         while lag >= MS_PER_UPDATE {
             v_y += gravity;
-            
+
             x += v_x;
             y += v_y;
 
             if y > 290.0 {
                 y = 290.0;
                 v_y = 0.0;
-                
+
                 on_ground = true;
             }
 
@@ -101,10 +101,10 @@ fn main() {
 
         player.x = x as i32;
         player.y = y as i32;
-        
+
         let _ = renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 255, 0));
         let _ = renderer.fill_rect(&player);
-        
+
         renderer.present();
     }
 
